92b050a06bddc82df1f0bd4f1f8fc67f5fc2a371
Ray Zhu <ray.zhu1@hp.com>
2020-09-23 10:30:43 +0000

    Add pool 'nagger'

---
+++ pool_of_processes/ppool_nagger.erl
@@ -0,0 +1,11 @@
+-module(ppool_nagger).
+-behavior(gen_server).
+-export([start_link/4, stop/1]).
+-export([init/1, handle_call/3, handle_cast/2,
+         handle_info/2, code_change/3, terminate/2]).
+
+start_link(Task, Deplay, Max, SendTo) ->
+    gen_server:start_link(?MODULE, {Task, Delay, Max, SendTo}, []).
+
+stop(Pid) ->
+    gen_server:call(Pid, stop).

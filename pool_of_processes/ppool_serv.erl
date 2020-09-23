9a724988f437321a6a60afaf11c5f212270c4972
Ray Zhu <ray.zhu1@hp.com>
2020-09-23 09:22:43 +0000

    Define MFA spec and server state. Wrong init

--- pool_of_processes/ppool_serv.erl
+++ pool_of_processes/ppool_serv.erl
@@ -3,6 +3,19 @@
 -export([start/4, start_link/4, run/2, sync_queue/2, async_queue/2, stop/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 
+-define(SPEC(MFA),
+        {worker_sup,
+         {ppool_worker, start_link, [MFA]},
+         temporary,
+         10000,
+         supervisor,
+         [ppool_worker_sup]})
+
+-record(state, {limit=0,
+               sup,
+               refs,
+               queue=queue:new()}).
+
 start(Name, Limit, Sup, MFA) when is_atom(Name), is_integer(Limit) ->
     gen_server:start({local, Name}, ?MODULE, {Limit, MFA, Sup}, []).
 
@@ -20,3 +33,8 @@ async_queue(Name, Args) ->
 
 stop(Name) ->
     gen_server:call(Name, stop).
+
+init({Limit, MFA, Sup}) ->
+    {ok, Pid} = supervisor:start_child(Sup, ?SPEC(MFA)),
+    link(Pid),
+    {ok, #state{limit=Limit, refs=gb_sets:empty()}}.

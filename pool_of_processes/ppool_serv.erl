59ad9a5e56e9dc04b243d010f3bbd1b2b34d16c7
Ray Zhu <ray.zhu1@hp.com>
2020-09-23 09:43:23 +0000

    Handle `run`

--- pool_of_processes/ppool_serv.erl
+++ pool_of_processes/ppool_serv.erl
@@ -45,3 +45,10 @@ handle_info({start_worker_supervisor, Sup, MFA}, S = #state{}) ->
 handle_info(Msg, State) ->
     io:format("Unknown message: ~p~n", [Msg]),
     {noreply, State}.
+
+handle_call({run, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0 ->
+    {ok, Pid} = supervisor:start_child(Sup, Args),
+    Ref = erlang:monitor(process, Pid),
+    {reply, {ok, Pid}, S#state{limit=N-1, refs=fb_sets:add(Ref, R)}};
+handle_call({run, _Args}, _From, S = #state{limit=N}) when N =< 0 ->
+    {reply, noalloc, S};

9cdfd38e30ed65964f2962c0b5367738d97254ea
Ray Zhu <ray.zhu1@hp.com>
2020-09-23 09:49:19 +0000

    Handle sync call
    When we still have space, start a worker to do that.
    Else, put the order in the queue.

--- pool_of_processes/ppool_serv.erl
+++ pool_of_processes/ppool_serv.erl
@@ -52,3 +52,9 @@ handle_call({run, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0
     {reply, {ok, Pid}, S#state{limit=N-1, refs=fb_sets:add(Ref, R)}};
 handle_call({run, _Args}, _From, S = #state{limit=N}) when N =< 0 ->
     {reply, noalloc, S};
+handle_call({sync, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0 ->
+    {ok, Pid} = supervisor:start_child(Sup, Args),
+    Ref = erlang:monitor(process, Pid),
+    {reply, {ok, Pid}, S#state{limit=N-1, refs=gb_sets:add(Ref, R)}};
+handle_call({sync, Args}, From, S = #state{queue=Q}) ->
+    {noreply, S#state{queue=queue:in({From, Args}, Q)}};

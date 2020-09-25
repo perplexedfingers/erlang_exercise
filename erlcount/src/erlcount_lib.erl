4f6fba0fed2bf6e984efd187a5197cf8483f980e
Ray Zhu <ray.zhu1@hp.com>
2020-09-25 08:57:37 +0000

    Add 'regex_count'

--- erlcount/src/erlcount_lib.erl
+++ erlcount/src/erlcount_lib.erl
@@ -1,5 +1,5 @@
 -module(erlcount_lib).
--export([find_erl/1]).
+-export([find_erl/1, regex_count/2]).
 -include_lib("kernel/include/file.hrl").
 
 find_erl(Directory) ->
@@ -37,4 +37,10 @@ handle_regular_file(Name, Queue) ->
             {continue, Name, fun() -> dequeue_and_run(Queue) end};
         _NonErl ->
             dequeue_and_run(Queue)
+    end.
+
+regex_count(Re, Str) ->
+    case re:run(Str, Re, [global]) of
+        nomatch -> 0;
+        {match, List} -> length(List)
     end.
